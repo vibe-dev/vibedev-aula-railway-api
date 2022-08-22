2f788ed61c29cdf6fec8c5e1d45fef9d0353726c
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2022-08-22 14:33:07 -0300

    feat: update

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const client_1 = require("@prisma/client");
 const server = (0, express_1.default)();
 const prisma = new client_1.PrismaClient();
 server.use((0, cors_1.default)());
